41cec7b3e1fb561a5b2c92c5cd627b0e4642a0a5
Anderson Jader <andersonjader036@gmail.com>
2024-03-12 13:15:38 -0300

    Fixed operacoes >> operations and creations of the folders to organization

---
+++ Exercises/mod.01/exercise02.py
@@ -0,0 +1,10 @@
+
+valor_compras = 51.5   
+desconto = 0.03            
+quantidade_itens = 22
+
+valor_final = valor_compras - (desconto * valor_compras)
+custo_medio = valor_compras / quantidade_itens
+
+print("Valor final das compras:",valor_final)
+print("Custo médio de cada item:",custo_medio)
