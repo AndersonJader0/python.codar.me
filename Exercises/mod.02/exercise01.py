815a1def3265e54fde263c4bfab628803b7d163d
Anderson Jader <andersonjader036@gmail.com>
2024-03-13 10:48:34 -0300

    new exercise

---
+++ Exercises/mod.02/exercise01.py
@@ -0,0 +1,5 @@
+x = int(input("Digite um número inteiro: "))
+
+ispar = x % 2
+
+print("O número digitado é par:", ispar == 0)
---
+++ Exercises/mod.02/exercise02.py
@@ -0,0 +1,10 @@
+resultado1 = 2 + 3 * 2 ** 2
+print(resultado1)
+
+
+resultado2 = 2 + ((3 * 2) ** 2)
+print(resultado2)
+
+
+resultado3 = ((2 + 3) * 2) ** 2
+print(resultado3)
