c73369bf0aa681d29ce7deb646ce96911363ef22
Anderson Jader <andersonjader036@gmail.com>
2024-03-14 23:48:29 -0300

    new class

---
+++ Classes/Data Structure/list.py
@@ -0,0 +1,18 @@
+notas = [8, 10, 8.5]
+
+notas.append(9)
+
+print(notas)
+notas.sort(reverse=True)
+print(notas)
+
+x = notas.pop()
+print(notas)
+print("x", x)
+
+notas.insert(0, 8)
+print("após inserção")
+print(notas)
+
+notas.pop(0)
+print(notas)
